b4233c13e14c9be7e4fc4e9cee1934e659d11a7e
alfred17oo <heca10_156@hotmail.com>
2020-02-21 21:44:31 -0600

    primer commit

---
+++ src/app/shared/nopegefound/nopegefound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopegefound',
+  templateUrl: './nopegefound.component.html',
+  styles: []
+})
+export class NopegefoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
